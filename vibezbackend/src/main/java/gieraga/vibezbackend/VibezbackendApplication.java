aaf934b51b16c973e1c7c193e8227d55ab46cfe6
SzymonGieraga <245809@edu.p.lodz.pl>
2025-10-02 19:50:33 +0200

    Initial commit + Video playing testing

---
+++ vibezbackend/src/main/java/gieraga/vibezbackend/VibezbackendApplication.java
@@ -0,0 +1,13 @@
+package gieraga.vibezbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibezbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VibezbackendApplication.class, args);
+    }
+
+}
